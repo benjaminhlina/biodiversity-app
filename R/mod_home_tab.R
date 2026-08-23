3750fc0e052cbc642b91bf49350cb296e18026da
benjaminhlina <benjamin.hlina@gmail.com>
2026-08-23 12:02:40 -0400

    make home server get data still having issues with map displaying but the data is coming through right

--- R/mod_home_tab.R
+++ R/mod_home_tab.R
@@ -16,6 +16,11 @@ home_tab_ui <- function(id) {
     tabName = "home",
     shiny::h2(
       "Welcome "
+    ),
+    shinycssloaders::withSpinner(
+      leaflet::leafletOutput(ns("map"), height = "700px", width = "100%"),
+      type = 4,
+      caption = "Please wait for the map to load..."
     )
   )
 }
@@ -29,15 +34,65 @@ home_server <- function(id, con, main_input, home_sidebar_vals) {
       )
     })
 
+    map_dat <- shiny::reactive({
+      shiny::req(home_sidebar_vals())
+      get_map_data(con = con, input = home_sidebar_vals())
+    })
+
+    # Debugging observer - add inside home_server
+
+    map_data_complete <- shiny::reactive({
+      shiny::req(map_dat())
+
+      data <- map_dat() |>
+        dplyr::mutate(
+          popup_info = paste0(
+            "<b>Area:</b> ",
+            continent,
+            "<br>",
+            "<b>Country:</b> ",
+            country,
+            "<br>",
+            "<b>Scientific Name:</b> ",
+            display_label,
+            "<br>",
+            "<b>Life stag:</b> ",
+            life_stage,
+            "<br>",
+            "<b>Sex:</b> ",
+            sex,
+            "<br>",
+            "<b>Collector Name:</b> ",
+            creator,
+            "<br>",
+            "<b>n:</b> ",
+            individual_count
+          )
+        ) |>
+        dplyr::collect()
+    })
+
+    shiny::observe({
+      cli::cli_inform(
+        "Checking sidebar vals: {.val {is.null(home_sidebar_vals())}}"
+      )
+
+      cli::cli_inform(
+        "map_dat returned row count: {.val {nrow(map_data_complete())}}"
+      )
+    })
+
     output$map <- leaflet::renderLeaflet({
-      # Check if the table exists before proceeding
-      if (!"tbl_location" %in% DBI::dbListTables(con)) {
-        return(
-          leaflet::leaflet() |>
-            leaflet::addTiles() |>
-            leaflet::addMarkers(lng = 0, lat = 0, popup = "No Data")
+      df <- map_data_complete()
+      shiny::req(nrow(df) > 0)
+
+      leaflet::leaflet(df) |>
+        leaflet::addTiles() |>
+        leaflet::addCircleMarkers(
+          lng = ~longitude_decimal,
+          lat = ~latitude_decimal,
+          popup = ~popup_info
         )
-      }
     })
   })
 }
