637abb4288474604b55fa3a8bb41ccb73b61e4a6
benjaminhlina <benjamin.hlina@gmail.com>
2026-08-23 09:23:28 -0400

    remove fluff

--- R/fnct_startup.R
+++ R/fnct_startup.R
@@ -1,29 +1,17 @@
 #' Start up GLATAR App
 #'
 #' This function, which has no arguments, starts the database
-#' connection, gets `valid_values`` for validation, gets
-#' `nice_names` for properly displaying tables, creates
-#' credientials, and and file location for resources.
+#' connection, dispalys the ap verions.
 #'
 #' @export
 start_up <- function() {
-  # ---- get _valid_values from db -------
+  # ---- start db connecitons  db -------
   con <- start_db_con()
   app_version <- "0.1.0"
-
-  # create named vectors
-  # nice_name_lookup <- stats::setNames(
-  #   naming_conventions$nice_names,
-  #   naming_conventions$raw_names
-  # )
-  # ----- load everything ------
-  # gtag_path <- system.file("js", package = "glatar")
 
   startup <- list(
     con = con,
     app_version = app_version
-    # naming_conventions = naming_conventions,
-    # nice_name_lookup = nice_name_lookup
   )
   return(startup)
 }
