ab1bdfce336af610316e5bbc3484cc0730be66be
benjaminhlina <benjamin.hlina@gmail.com>
2026-08-25 10:15:37 -0400

    update mod home with new create functions and siplay functions

--- R/mod_home_tab.R
+++ R/mod_home_tab.R
@@ -65,27 +65,9 @@ home_server <- function(id, con, main_input, home_sidebar_vals) {
       data = base_map_dat,
       output = output,
       output_id = "date_range_ui",
-      session =session
+      session = session
     )
-    # output$date_range_ui <- shiny::renderUI({
-    #   shiny::req(base_map_dat())
-    #   df <- base_map_dat()
-    #   shiny::req(!is.null(df), nrow(df) > 0)
-    #   shiny::req(!all(is.na(df$date_time)))
 
-    #   dt_min <- min(df$date_time, na.rm = TRUE)
-    #   dt_max <- max(df$date_time, na.rm = TRUE)
-
-    #   shiny::sliderInput(
-    #     inputId = ns("date_range"),
-    #     label = "Filter by the date and time observered",
-    #     min = dt_min,
-    #     max = dt_max,
-    #     value = c(dt_min, dt_max),
-    #     timeFormat = "%Y-%m-%d %H:%M",
-    #     width = "100%"
-    #   )
-    # })
     # ----- filter data based on slider inputs -----
     final_map_dat <- create_filtered_data(data = base_map_dat, input = input)
 
@@ -100,39 +82,7 @@ home_server <- function(id, con, main_input, home_sidebar_vals) {
       )
     })
     # ----- output create map ------
-    # -----
-    output$map <- leaflet::renderLeaflet({
-      df <- final_map_dat()
-      # shiny::req(nrow(df) > 0)
 
-      pal <- leaflet::colorNumeric(
-        palette = "viridis",
-        domain = df$individual_count
-      )
-
-      map <- base_map(df)
-      if (!is.null(df) && nrow(df) > 0) {
-        map <- map |>
-          leaflet::addCircleMarkers(
-            lng = ~longitude_decimal,
-            lat = ~latitude_decimal,
-            popup = ~popup_info,
-            radius = ~ scales::rescale(sqrt(individual_count), to = c(4, 20)),
-            fillColor = ~ pal(individual_count),
-            fillOpacity = 0.8,
-            stroke = TRUE,
-            weight = 1,
-            color = "black"
-          ) |>
-          leaflet::addLegend(
-            position = "bottomright",
-            pal = pal,
-            values = ~individual_count,
-            title = "Individuals Observed",
-            opacity = 0.8
-          )
-      }
-      map
-    })
+    display_map(data = final_map_dat, output = output, output_id = "map")
   })
 }
