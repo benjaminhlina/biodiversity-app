87dd6c03e063b80aab258e7a227761ebf0f7a7ca
benjaminhlina <benjamin.hlina@gmail.com>
2026-08-25 14:15:51 -0400

    fix data documentation

--- R/data.R
+++ R/data.R
@@ -12,14 +12,25 @@
 #' }
 "countries"
 
-#' A `data.frame` containing the four species from poland used for testing
+#' A `data.frame` containing the names four species from poland
+#'
+#' @format `data.frame` containing 4 rows and 4 variables
+#'  \describe{
+#'    \item{country}{The country name)}
+#'    \item{scientific_name}{The scientific name}
+#'    \item{vernacular_name}{The vernacular name}
+#'    \item{display_label}{The combined of scientific and vernacular name}
+#' }
+"test_poland_spp"
+
+#' A `data.frame` containing occupancy for four species from poland used for testing
 #'
 #'
 #' @format `data.frame` containing 10 rows and 42 variables
 #'
 "test_poland_occ"
 
-#' A `data.frame` containing the four species from poland used for testing
+#' A `data.frame` containing the multimedia four species from poland used for testing
 #'
 #'
 #' @format `data.frame` containing 4 rows and 9 variables
