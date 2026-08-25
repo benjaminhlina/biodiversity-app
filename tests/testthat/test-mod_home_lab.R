5ee5afe836d164011124fe02b15f4f7bd425d567
benjaminhlina <benjamin.hlina@gmail.com>
2026-08-25 14:26:24 -0400

    tests need to updated

--- tests/testthat/test-mod_home_lab.R
+++ tests/testthat/test-mod_home_lab.R
@@ -66,15 +66,14 @@ test_that("create_filtered_data filters correctly by date_range", {
     expr = {
       df_filtered <- filtered_data()
 
-      expect_equal(nrow(df_filtered), 5)
+      expect_equal(nrow(df_filtered), 6)
       expect_equal(
         df_filtered$id,
         c(
-          '161827036@OBS',
-          '157461072@OBS',
-          '157474169@OBS',
-          '159676137@OBS',
-          '170864085@OBS'
+          "140960743@OBS",
+          "161827036@OBS",
+          "157461072@OBS",
+          "157474169@OBS"
         )
       )
     }
